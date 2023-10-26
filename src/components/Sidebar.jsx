453a2b75443db38ce472f0807dc85eab60aea4ce
Deniansyah <deniansyah814@gmail.com>
2023-10-27 01:16:23 +0700

    add feature logout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,22 @@
 "use client"
-import { LuHome, LuArchive, LuSettings, LuLogOut, LuLayoutGrid, LuUsers, LuArrowRightLeft } from "react-icons/lu";
 import Link from "next/link";
+import { LuHome, LuArchive, LuSettings, LuLogOut, LuLayoutGrid, LuUsers, LuArrowRightLeft } from "react-icons/lu";
+import { logout as LogoutAction } from "../store/auth/reducer";
+import { useDispatch } from "react-redux";
 
 const Sidebar = (props) => {
+  const dispatch = useDispatch();
+
   const dashboard = "dashboard";
   const transaction = "transaction";
   const product = "product";
   const category = "category";
   const user = "user";
   const profile = "profile";
+
+  const Logout = () => {
+    dispatch(LogoutAction());
+  };
 
   return (
     <div className="group hover:w-40 hover:shadow-2xl hover:items-start flex flex-col fixed h-screen gap-3 w-20 px-4 py-5 hover:px-0 border-r bg-[#fbbd23] rounded-tr-2xl rounded-br-2xl items-center z-10">
@@ -74,10 +82,10 @@ const Sidebar = (props) => {
       </Link>
       <div className="grow" />
       <div className="flex group-hover:px-4 justify-center items-center text-red-500 hover:bg-orange-400 rounded-full group-hover:rounded-l-none w-12 h-12 group-hover:w-full group-hover:justify-start">
-        <Link href={"/login"} className="flex justify-center items-center gap-5">
+        <button className="flex justify-center items-center gap-5" onClick={Logout}>
           <LuLogOut className="text-2xl" />
           <p className="hidden group-hover:block">Log out</p>
-        </Link>
+        </button>
       </div>
     </div>
   );
